770f54fb6b121cc6be7e5cc29b1eb7d698e82192
OudomMunint <108245473+OudomMunint@users.noreply.github.com>
2023-01-29 16:26:04 +1100

    added MCT and updated csproj

--- MauiProgram.cs
+++ MauiProgram.cs
@@ -1,24 +1,28 @@
 ﻿using Microsoft.Extensions.Logging;
+using CommunityToolkit.Maui;
 
 namespace redsix;
 
+using CommunityToolkit.Maui;
+
 public static class MauiProgram
 {
-	public static MauiApp CreateMauiApp()
-	{
-		var builder = MauiApp.CreateBuilder();
-		builder
-			.UseMauiApp<App>()
-			.ConfigureFonts(fonts =>
-			{
-				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
-				fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
-			});
+    public static MauiApp CreateMauiApp()
+    {
+        var builder = MauiApp.CreateBuilder();
+        builder
+            .UseMauiApp<App>()
+            // Initialize the .NET MAUI Community Toolkit by adding the below line of code
+            .UseMauiCommunityToolkit()
+            // After initializing the .NET MAUI Community Toolkit, optionally add additional fonts
+            .ConfigureFonts(fonts =>
+            {
+                fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+            });
 
-#if DEBUG
-		builder.Logging.AddDebug();
-#endif
+        // Continue initializing your .NET MAUI App here
 
-		return builder.Build();
-	}
+        return builder.Build();
+    }
 }
