bb0637ebfec5b502732d17dfbdaad2702bec6c91
OudomMunint <108245473+OudomMunint@users.noreply.github.com>
2023-02-07 19:38:21 +1100

    Camera Logic

--- ViewModels/CameraVm.cs
+++ ViewModels/CameraVm.cs
@@ -17,4 +17,78 @@ public partial class ProfileOptionsPageVm : RedSixBaseVm
     {
 
     }
+    public override void PageAppearing()
+    {
+        Console.WriteLine($"ProfilePrivacyPageViewModel PageAppearing");
+    }
+
+    [RelayCommand]
+    private void SettingsButtonPressed()
+    {
+        NavigationUtil.Navigate<SettingsPage>();
+    }
+
+    [RelayCommand]
+    private void PrivacySettingsButtonPressed()
+    {
+        NavigationUtil.Navigate<PrivacySettingsPage>();
+    }
+
+    [RelayCommand]
+    private void MoodHistoryButtonPressed()
+    {
+        NavigationUtil.Navigate<MoodHistoryPage>();
+    }
+
+    [RelayCommand]
+    private void DoneButtonPressed()
+    {
+        NavigationUtil.Navigate<SocialMainPage>();
+    }
+
+    [RelayCommand]
+    private async void TakePhotoPressed()
+    {
+        NavigationUtil.Navigate<PhotoPickPage>();
+    }
+
+
+    [RelayCommand]
+    private async void TakePhotoPressed2()
+    {
+        MediaPickerOptions options = new MediaPickerOptions();
+        options.Title = "Add Attachment";
+
+        // Convert FileResult from selected image to Base64 string
+        FileResult result = await MediaPicker.Default.PickPhotoAsync(options);
+        Stream imageStream = await result.OpenReadAsync();
+        byte[] imageBytes;
+        using (var memoryStream = new MemoryStream())
+        {
+            imageStream.CopyTo(memoryStream);
+            imageBytes = memoryStream.ToArray();
+        }
+
+        string imageDataString = Convert.ToBase64String(imageBytes);
+
+        ImageSource imageSource = ImageSource.FromFile(result.FullPath);
+
+        RedSixData data = new RedSixData();
+        //data.SetProfilePhoto(imageBytes);
+
+        //SetProfilePhotoDTO ua = new SetProfilePhotoDTO();
+        //ua.ProfileImageThumbnail =  imageBytes;
+
+        var response = await data.SetProfilePhoto(imageBytes);
+
+        if ((response != null) && (response.Success))
+        {
+            //Feed.Insert(0, response.Data);
+            //NewFeedContent = string.Empty;
+        }
+        else
+            ErrorHandler.HandleBaseResponseError<bool>(response);
+
+
+    }
 }
