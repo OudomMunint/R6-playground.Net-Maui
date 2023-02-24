069ad6611cfffa70fb820382ba66349a696ede9b
OudomMunint <108245473+OudomMunint@users.noreply.github.com>
2023-02-24 20:05:01 +1100

    Added graph VM

---
+++ ViewModels/GraphVm.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace redsix.ViewModels
+{
+
+    public class Sales
+    {
+        public string Product { get; set; }
+        public double SalesRate { get; set; }
+    }
+
+    class GraphVm
+    {
+        public List<Sales> Data { get; set; }
+
+        public GraphVm()
+        {
+            Data = new List<Sales>()
+        {
+            new Sales(){Product = "Happy", SalesRate = 5},
+            new Sales(){Product = "Okay", SalesRate = 1},
+            new Sales(){Product = "Sad", SalesRate = 1},
+            new Sales(){Product = "Need Help", SalesRate = 1},
+            new Sales(){Product = "Can't Decide", SalesRate = 2},
+        };
+        }
+    }
+ 
+}
