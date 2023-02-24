ba69ba9bf98712708dcfed3f2a791fcb0afdc57d
OudomMunint <108245473+OudomMunint@users.noreply.github.com>
2023-02-24 20:05:11 +1100

    Graph page and code behind

---
+++ Views/Graph.xaml.cs
@@ -0,0 +1,28 @@
+using redsix.ViewModels;
+using Syncfusion.Maui.Charts;
+
+namespace redsix.Views;
+
+public partial class Graph : ContentPage
+{
+    public Graph()
+    {
+        this.InitializeComponent();
+
+        SfCircularChart chart = new SfCircularChart();
+
+        GraphVm viewModel = new GraphVm();
+        chart.BindingContext = viewModel;
+
+        chart.Legend = new ChartLegend();
+        PieSeries series = new PieSeries();
+        series.ItemsSource = viewModel.Data;
+        series.XBindingPath = "Product";
+        series.EnableTooltip = true;
+        series.ShowDataLabels = true;
+        series.YBindingPath = "SalesRate";
+        chart.Series.Add(series);
+
+        Content = chart;
+    }
+}
