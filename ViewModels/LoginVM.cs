f2eb4e5790caf52c70b45246a6f26421cd84da07
OudomMunint <108245473+OudomMunint@users.noreply.github.com>
2023-04-17 14:45:42 +1000

    Show password switch

--- ViewModels/LoginVM.cs
+++ ViewModels/LoginVM.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,29 @@ namespace redsix.ViewModels
 {
     internal class LoginVM
     {
+        public object ShowPasswordSwitch { get; private set; }
+
+        //private void ShowPasswordSwitch_Toggled(object sender, ToggledEventArgs e)
+        //{
+        //    ShowPasswordSwitch.Text = e.Value ? "Hide password" : "Show password";
+        //}
+
+        public class InvertedBoolConverter : IValueConverter
+        {
+            public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+            {
+                if (value is bool boolValue)
+                {
+                    return !boolValue;
+                }
+
+                return value;
+            }
+
+            public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+            {
+                throw new NotImplementedException();
+            }
+        }
     }
 }
--- Views/Login.xaml.cs
+++ Views/Login.xaml.cs
@@ -1,9 +1,12 @@
+using System.Globalization;
+
 namespace redsix.Views;
 
 public partial class Login : ContentPage
 {
 	public Login()
 	{
-		InitializeComponent();
+        InitializeComponent();
 	}
+
 }
